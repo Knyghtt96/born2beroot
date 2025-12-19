1b41d3cf42689e7f31050f16066c2546a96cdde2
Knyghtt96 <debruycker.mike@gmail.com>
2025-12-19 15:42:43 +0100

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+arch=$(uname -a)
+
+cpuphy=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+
+cpuvir=$(nproc)
+
+#memfree= $(free -m | awk '$1 == "Mem:" {print $2}') #display free/unused memory
+#memused= $(free -m | awk '$1 == "Mem:" {print $3}')
+#memcat=  $(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+#memstat= $(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+
+memfree=$(free -m | grep Mem | awk '{print $3}')
+memused=$(free -m | grep Mem | awk '{print $2}')
+memtotal=$(free -m | grep Mem | awk '{print $3/$2 * 100}')
+memstat="${memfree}/${memused}MB ($(printf "%.2f" $memtotal)%)"
+
+
+
+wall"	
+	#Architecture: $arch
+	#CPU Physical: $cpuphy
+	#vCPU:$cpuvir
+	#Memmory Usage: $memstat
+	#Disk Usage:
+	#CPU load:
+	#Last boot:
+	#LVM use:
+	#Connections TCP:
+	#User log:
+	#Network:
+	#Sudo: 
+"
+
+#https://42-cursus.gitbook.io/guide/1-rank-01/born2beroot/p2p-evaluation-questions
+#https://www.gosquared.com/blog/vi-linux-terminal-help-sheet
+#https://github.com/chlimous/42-born2beroot_guide/blob/main/monitoring.sh
+#https://github.com/chlimous/42-born2beroot_guide?tab=readme-ov-file
