b2ef6ae28b86b56a3c675952de63ed5a21734ef6
Knyghtt96 <debruycker.mike@gmail.com>
2026-01-16 15:34:20 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -6,10 +6,10 @@ cpuphy=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
 
 cpuvir=$(nproc)
 
-memfree=$(free -m | grep Mem | awk '{print $3}')
-memused=$(free -m | grep Mem | awk '{print $2}')
-memtotal=$(free -m | grep Mem | awk '{print $3/$2 * 100}')
-memstat="${memfree}/${memused}MB ($(printf "%.2f" $memtotal)%)"
+memused=$(free -m | awk '/Mem:/ {print $3}')
+memtotal=$(free -m | awk '/Mem:/ {print $2}')
+mempercent=$(free -m | awk '/Mem:/ {printf "%.2f", $3/$2*100}')
+memstat="${memused}/${memtotal}MB (${mempercent}%)"
 
 diskuse=$(df -BG / | tail -n 1 | awk '{print $3}' | tr -d G)
 disktot=$(df -BG / | tail -n 1 | awk '{print $2}' | tr -d G)
@@ -17,24 +17,25 @@ diskpercent=$(df -h / | tail -n 1 | awk '{print $5}')
 
 diskstat="${diskuse}/${disktot}GB (${diskpercent})"
 
-cpuusr=$(mpstat 1 1 | awk '/^Average: +all/ {print $4}')
-cpusyst=$(mpstat 1 1 | awk '/^Average: +all/ {print $6}')
-cputot=$(echo "$cpusr + $cpusys" | bc)
-cputot=$(echo "${cputot}%")
+cputot=$(mpstat 1 1 | awk '/Average:/ {printf "%.1f", 100 - $12}')
+
+boot=$(who -b | awk '{print $3 " " $4}')
+
+lvm=$(cat /etc/fstab | grep /dev/mapper | wc -l)
 
 wall "
-	#Architecture: $arch
-	#CPU Physical: $cpuphy
-	#vCPU:$cpuvir
-	#Memmory Usage: $memstat
-	#Disk Usage: $diskstat
-	#CPU load: $cputot
-	#Last boot:
-	#LVM use:
-	#Connections TCP:
-	#User log:
-	#Network:
-	#Sudo:
+#Architecture: $arch
+#CPU Physical: $cpuphy
+#vCPU:$cpuvir
+#Memory Usage: $memstat
+#Disk Usage: $diskstat
+#CPU load: $cputot%
+#Last boot:$boot
+#LVM use:$lvm
+#Connections TCP:
+#User log:
+#Network:
+#Sudo:
 "
 
 #https://42-cursus.gitbook.io/guide/1-rank-01/born2beroot/p2p-evaluation-questions
