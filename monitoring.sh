3de2d44c8cd52be0d45ae553eea3e47b01d50121
Knyghtt96 <debruycker.mike@gmail.com>
2026-01-16 16:08:10 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,7 +3,6 @@
 arch=$(uname -a)
 
 cpuphy=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-
 cpuvir=$(nproc)
 
 memused=$(free -m | awk '/Mem:/ {print $3}')
@@ -14,7 +13,6 @@ memstat="${memused}/${memtotal}MB (${mempercent}%)"
 diskuse=$(df -BG / | tail -n 1 | awk '{print $3}' | tr -d G)
 disktot=$(df -BG / | tail -n 1 | awk '{print $2}' | tr -d G)
 diskpercent=$(df -h / | tail -n 1 | awk '{print $5}')
-
 diskstat="${diskuse}/${disktot}GB (${diskpercent})"
 
 cputot=$(mpstat 1 1 | awk '/Average:/ {printf "%.1f", 100 - $12}')
@@ -23,25 +21,26 @@ boot=$(who -b | awk '{print $3 " " $4}')
 
 lvm=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
 
+tcp=$(ss -t state established | tail -n +2 | wc -l)
 
+ulog=$(users | wc -w)
 
+ip=$(hostname -I | awk '{print $1}')
+mac=$(ip link show | awk '/ether/ {print $2}')
+
+sudo=$(grep "COMMAND=" /var/log/sudo/sudo.log | wc -l)
 
 wall "
 #Architecture: $arch
 #CPU Physical: $cpuphy
-#vCPU:$cpuvir
+#vCPU: $cpuvir
 #Memory Usage: $memstat
 #Disk Usage: $diskstat
 #CPU load: $cputot%
-#Last boot:$boot
-#LVM use:$lvm
-#Connections TCP:
-#User log:
-#Network:
-#Sudo:
+#Last boot: $boot
+#LVM use: $lvm
+#Connections TCP: $tcp ESTABLISHED
+#User log: $ulog
+#Network: IP $ip (MAC $mac)
+#Sudo: $sudo
 "
-
-#https://42-cursus.gitbook.io/guide/1-rank-01/born2beroot/p2p-evaluation-questions
-#https://www.gosquared.com/blog/vi-linux-terminal-help-sheet
-#https://github.com/chlimous/42-born2beroot_guide/blob/main/monitoring.sh
-#https://github.com/chlimous/42-born2beroot_guide?tab=readme-ov-file
